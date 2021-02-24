08ec1014d682f2d6548d6dcdf50b5fa540fe017b
CorneliusJason <cornelius.jason@gmail.com>
2021-02-24 16:10:54 +0700

    add desktop and mobile vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
+    <v-main
+      v-if="$vuetify.breakpoint.width < 414"
+    >
+      <Mobile/>
+    </v-main>
+
+    <v-main
+      v-else
+    >
+      <Desktop/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Mobile from './components/Mobile';
+import Desktop from './components/Desktop';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Mobile,
+    Desktop
   },
 
   data: () => ({
---
+++ src/components/Desktop.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card>
+    <h1>Desktop</h1>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Desktop',
+  }
+</script>
